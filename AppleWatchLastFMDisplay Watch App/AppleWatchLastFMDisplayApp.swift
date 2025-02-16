ea151176b7d4c4ad089376a3a1ca087a962afa94
Tom Planche <Tomplanche@icloud.com>
2025-02-16 22:43:32 +0100

    Initial Commit

---
+++ AppleWatchLastFMDisplay Watch App/AppleWatchLastFMDisplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleWatchLastFMDisplayApp.swift
+//  AppleWatchLastFMDisplay Watch App
+//
+//  Created by Tom Planche on 16/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppleWatchLastFMDisplay_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleWatchLastFMDisplay Watch AppTests/AppleWatchLastFMDisplay_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppleWatchLastFMDisplay_Watch_AppTests.swift
+//  AppleWatchLastFMDisplay Watch AppTests
+//
+//  Created by Tom Planche on 16/02/2025.
+//
+
+import Testing
+@testable import AppleWatchLastFMDisplay_Watch_App
+
+struct AppleWatchLastFMDisplay_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
